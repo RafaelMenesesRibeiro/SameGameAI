7c9ab6ff64f984f8a9a33a8b4995f65b6bba8794
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2017-10-05 16:40:29 +0000

    board.py : UNFINISHED

---
+++ Board.py
@@ -0,0 +1,61 @@
+from pandas import *
+
+class Board:
+    __colors = 0
+    __lines = 0
+    __filledLines = 0
+    __columns = 0
+    __filledColumns = 0
+    __boardMatrix = []
+
+    def __init__(self, lines, columns, colors):
+        self.__lines = lines
+        self.__columns = columns
+        self.__colors = colors
+
+    def get_board(self):
+        return self.__boardMatrix
+
+    def get_columns(self):
+        return self.__columns
+
+    def get_lines(self):
+        return self.__lines
+
+    def get_colors(self):
+        return self.__colors
+
+    def get_filled_lines(self):
+        return self.__filledLines
+
+    def get_filled_columns(self):
+        return self.__filledColumns
+
+    def set_filled_lines(self, n):
+        self.__filledLines = n
+
+    def set_filled_columns(self, n):
+        self.__filledColumns = n
+
+    def board_find_groups(self):
+        # TODO
+        pass
+
+    def board_remove_group(self, group):
+        # TODO
+        pass
+
+    def to_string(self):
+        print('Printing SameGame board with ', self.__lines, 'and ', self.__columns, '...')
+        print('  ', end='')
+        for i in range(self.__columns):
+            print(i, end=' ')
+        for l in self.__boardMatrix:
+            print()
+            print(l, end=' ')
+            for c in self.__boardMatrix[l]:
+                print(self.__boardMatrix[l][c], end=' ')
+        print('\nDone')
+
+
+
