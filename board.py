5106e641d8b54d4940be6b8f126194a8a9fc9fb8
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-10 17:43:59 +0100

    Started testing

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 from operator import itemgetter
 import random
-
+import search
 
 class Board:
     __colorsNumber = 0
@@ -27,12 +27,13 @@ class Board:
         return self.__colorsNumber
 
     def populate_board(self):
-        print('Populating game board...')
         for l in range(self.__columns):
+            line = []
             for c in range(self.__lines):
                 color = random.randint(1, self.__colorsNumber)
-                self.__boardMatrix[l][c] = color
-        print('Done')
+                line.append(color)
+            self.__boardMatrix.append(line)
+
 
     def is_empty(self):
         for l in range(self.__columns):
@@ -54,7 +55,7 @@ class Board:
         rightborder = self.__columns - 1
         # For each adjacent coordiante, chacks if it is valid.
         for adj in [(line - 1, column), (line + 1, column), (line, column - 1), (line, column + 1)]:
-            if bottomborder > adj[1] >= topborder and rightborder > adj[0] >= leftborder:
+            if bottomborder >= adj[1] >= topborder and rightborder >= adj[0] >= leftborder:
                 # If it is inside the board, adds it to the list.
                 adjacent.append(adj)
             # Returns the list of adjacent valid coordinates.
@@ -82,6 +83,11 @@ class Board:
             # Gets the board coordinates of the piece.
             line = nextposition[0]
             column = nextposition[1]
+
+
+
+            #print('rootLine {}, rootcolumn {}, current pos {} {}'.format(rootline, rootcolumn, line, column))
+
             # Gets the adjacent coordinates of the current one.
             adjacentballs = self.get_adjacent_coordinates(line, column)
             # For each adjacent coordinate checks if it meets the requirements
@@ -121,6 +127,7 @@ class Board:
                 # Checks if the current position in empty and if it is, checks
                 # if the position was already visited (in a previous BFS).
                 if self.__boardMatrix[i][j] != 0 and not visited[i][j]:
+                    #print('CALLING ROOT FIND WITH {} {}'.format(i, j))
                     # Gets the cluster of which the ball in the current position
                     # belongs to.
                     newcluster = self.root_find_group(visited, i, j)
@@ -187,74 +194,15 @@ class Board:
         return boardcopy
 
     def to_string(self):
-        print('Printing SameGame board with ', self.__lines, 'and ', self.__columns, '...')
-        print('  ', end='')
-        for i in range(self.__columns):
-            print(i, end=' ')
-        for l in self.__boardMatrix:
-            print()
-            print(l, end=' ')
-            for c in self.__boardMatrix[l]:
-                print(self.__boardMatrix[l][c], end=' ')
-        print('\nDone')
+        for l in range(self.__columns):
+            print('[ ', end='')
+            for c in range(self.__lines):
+                print('{} '.format(self.__boardMatrix[l][c]), end='')
+            print(']')
 
 
-
-class same_game(Problem):
-    #Models a Same Game problem as a satisfaction problem.
-    #A solution cannot have pieces left on the board.
-    def __init__(self, board):
-        initialstate = sg_state(board)
-        Problem.__init__(self, initialstate)
-
-    '''Return the actions that can be executed in the given
-    state. The result would typically be a list, but if there are
-    many actions, consider yielding them one at a time in an
-    iterator, rather than building them all at once.'''
-    def actions(self, state):
-        #Gets the board of the state.
-        board = state.board
-        #Find all the clusters in the board.
-        clusters = board.board_find_groups()
-        #Trims the clusters to only consider groups of 2 or more pieces.
-        validclusters = []
-        for cluster in cluster:
-            if cluster.is_valid_action():
-                validclusters.append(cluster)               
-        return validclusters
-
-    '''Return the state that results from executing the given
-    action in the given state. The action must be one of
-    self.actions(state).'''
-    def result(self, state, action):
-        #Gets the board of the state.
-        board = state.board
-        #Gets the cluster to be removed.
-        cluster = action
-        #Calculates the board that would result in completing the action in the
-        #given state.
-        resultingboard = board.board_remove_group(cluster)
-        return resultingboard
-
-    '''Return True if the state is a goal. The default method compares the
-    state to self.goal or checks for state in self.goal if it is a
-    list, as specified in the constructor. Override this method if
-    checking against a single self.goal is not enough.'''
-    def goal_test(self, state):
-        #Gets the board of the state.
-        board = state.board
-        if board.is_empty():
-            return True
-        return False
-
-    def path_cost(self, c, state1, action, state2):
-        '''Return the cost of a solution path that arrives at state2 from
-        state1 via action, assuming cost c to get up to state1. If the problem
-        is such that the path doesn't matter, this function will only look at
-        state2.  If the path does matter, it will consider c and maybe state1
-        and action. The default method costs 1 for every step in the path.'''
-        return c + 1
-
-    def h(self, node):
-        #Needed for informed search.
-        pass
+if __name__ == '__main__':
+    board = Board(5, 5, 3)
+    board.to_string()
+    clusters = board.board_find_groups()
+    print(clusters)
