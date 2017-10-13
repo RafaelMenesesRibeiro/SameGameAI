7194c9525a09e636ea4c8b5dd7b115949372a4cd
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-10-13 10:31:48 +0100

    less expensive version

--- samegameai.py
+++ samegameai.py
@@ -167,6 +167,53 @@ def board_find_valid_groups(board):
 	return clusters
 
 
+def root_find_number_cluster(board, visited, rootline, rootcolumn, lines, columns):
+	# Marks the root piece as visited.
+	visited[rootline][rootcolumn] = True
+	# Starts the queue with just the root piece.
+	queue = [(rootline, rootcolumn)]
+	# Gets the root color so it doens't need to access the matrix even more
+	# times.
+	rootcolor = get_color(board, rootline, rootcolumn)
+	while len(queue) > 0:
+		# Removes a piece from the list.
+		nextposition = queue.pop()
+		# Gets the board coordinates of the piece.
+		l = pos_l(nextposition)
+		c = pos_c(nextposition)
+		# Gets the adjacent coordinates of the current one.
+		adjacentballs = get_adjacent_coordinates(l, c, lines, columns)
+		# For each adjacent coordinate checks if it meets the requirements to be added to the cluster.
+		for pos in adjacentballs:
+			# Gets the adjacent coordinates.
+			l = pos_l(pos)
+			c = pos_c(pos)
+			# Checks if the coordinate is not empty and if the coordinate's piece is the same color as the root piece.
+			if not visited[l][c] and rootcolor == get_color(board, l, c):
+				# In case the requirements are met, adds the adjacent piece to the cluster and to the queue
+				queue.append((l, c))
+				# Sets the visitation flag to True so it doesnt get added again.
+				visited[l][c] = True
+			# Returns the list of coordinates that are adjacent to each other and are the same color as the root.
+	return 1
+
+def board_find_number_groups(board):
+	lines = len(board)
+	columns = len(board[0])
+	# Creates the matrix that represents if a position has been checked for or has been added to a cluster.
+	# Initiates all to False because no position was visited yet.
+	visited = [[False for _ in range(columns)] for _ in range(lines)]
+	# For each valid (non empty) position on the board, get its cluster.
+	counter = 0
+	for l in range(lines):
+		for c in range(columns):
+			# Checks if the current position in empty and if it is, checks if the it was already visited in a previous BFS.
+			if color(get_color(board, l, c)) and not visited[l][c]:
+				# Get the cluster to which the ball in this position belongs to.
+				counter += root_find_number_cluster(board, visited, l, c, lines, columns)
+	return counter
+
+
 def concatenate_lines(boardcopy, cluster, index, lines):
 	displacement = 0
 	clusterindex = index
@@ -228,8 +275,8 @@ class sg_state:
 		return self.board
 
 	def __lt__(self, other_sg_state):
-		thisclusters = len(board_find_groups(self.get_board()))
-		otherclusters = len(board_find_groups(other_sg_state.get_board()))
+		thisclusters = board_find_number_groups(self.get_board())
+		otherclusters = board_find_number_groups(other_sg_state.get_board())
 		return thisclusters < otherclusters
 
 	def cluster_count(self):
@@ -280,7 +327,8 @@ class same_game(Problem):
 		return True
 
 	def path_cost(self, c, state1, action, state2):
-		return c + 1
+		numberremoved = len(action)
+		return c + numberremoved
 
 	def h(self, node):
 		board = node.state.get_board()
@@ -290,3 +338,6 @@ class same_game(Problem):
 				if board[i][j] != 0:
 					coloredballs += 1
 		return coloredballs
+
+
+print(depth_first_tree_search(same_game([[3,1,3,2],[1,1,1,3],[1,3,2,1],[1,1,3,3],[3,3,1,2],[2,2,2,2],[3,1,2,3],[2,3,2,3],[2,1,1,3],[2,3,1,2]])).state.board)
